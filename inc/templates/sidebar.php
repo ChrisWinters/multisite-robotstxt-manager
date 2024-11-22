ce0eeb10e6012387557c9c351565e26181de229b
Chris Winters <webmaster.skills@gmail.com>
2024-11-21 21:09:18 -0500

    Update sidebar links

--- inc/templates/sidebar.php
+++ inc/templates/sidebar.php
@@ -33,31 +33,13 @@ if ( false === defined( 'ABSPATH' ) ) {
 	<div class="h5 p-1 font-weight-bold"><?php esc_html_e( 'Robots.txt Manager', 'multisite-robotstxt-manager' ); ?></div>
 <div class="inside" style="clear:both;padding-top:1px;"><div class="para">
 	<ul>
-		<li class="font-weight-bold">&bull; <a href="<?php echo esc_url( network_site_url( 'wp-admin/network' ) ); ?>/settings.php?page=multisite-robotstxt-manager-affiliation"><?php esc_html_e( 'Affiliates Earn 50% Revshare', 'multisite-robotstxt-manager' ); ?></a></li>
-		<li>&bull; <a href="<?php echo esc_url( network_site_url( 'wp-admin/network' ) ); ?>/settings.php?page=multisite-robotstxt-manager-contact"><?php esc_html_e( 'Contact Support', 'multisite-robotstxt-manager' ); ?></a></li>
-		<li>&bull; <a href="https://wordpress.org/support/plugin/multisite-robotstxt-manager/" target="_blank"><?php esc_html_e( 'WordPress Forum', 'multisite-robotstxt-manager' ); ?></a></li>
 		<li>&bull; <a href="https://github.com/ChrisWinters/multisite-robotstxt-manager" target="_blank"><?php esc_html_e( 'Plugin Home Page', 'multisite-robotstxt-manager' ); ?></a></li>
 		<li>&bull; <a href="https://github.com/ChrisWinters/multisite-robotstxt-manager/issues" target="_blank"><?php esc_html_e( 'Bugs & Feature Requests', 'multisite-robotstxt-manager' ); ?></a></li>
 	</ul>
 </div></div> <!-- end inside-pad & inside -->
 </div> <!-- end postbox -->
 
-<div class="postbox">
-	<div class="h5 p-1 font-weight-bold"><?php esc_html_e( 'Notice', 'multisite-robotstxt-manager' ); ?>:</div>
-<div class="inside" style="clear:both;padding-top:1px;"><div class="para">
-
-	<?php esc_html_e( 'Please report any issues, bugs, or problems you have - your help is greatly appreciated.', 'multisite-robotstxt-manager' ); ?>
-
-	<ul>
-		<li><a href="<?php echo esc_url( network_site_url( 'wp-admin/network' ) ); ?>/settings.php?page=multisite-robotstxt-manager-contact"><?php esc_html_e( 'Direct Email', 'multisite-robotstxt-manager' ); ?></a></li>
-		<li><a href="https://github.com/ChrisWinters/multisite-robotstxt-manager/issues" target="_blank"><?php esc_html_e( 'Github Issues', 'multisite-robotstxt-manager' ); ?></a></li>
-		<li><a href="https://wordpress.org/support/plugin/multisite-robotstxt-manager/" target="_blank"><?php esc_html_e( 'WordPress Forum', 'multisite-robotstxt-manager' ); ?></a></li>
-	</ul>
-
-</div></div> <!-- end inside-pad & inside -->
-</div> <!-- end postbox -->
-
-<p><a href="https://wordpress.org/support/plugin/multisite-robotstxt-manager/reviews/?rate=5#new-post" target="_blank"><img src="<?php echo esc_url( plugin_dir_url( MS_ROBOTSTXT_MANAGER_FILE ) ); ?>/assets/images/sidebar_rate-plugin.gif" alt="<?php esc_html_e( 'Please Rate This Plugin At Wordpress.org!', 'multisite-robotstxt-manager' ); ?>" /></a></p>
+<p><a href="https://github.com/ChrisWinters/multisite-robotstxt-manager" target="_blank"><img src="<?php echo esc_url( plugin_dir_url( MS_ROBOTSTXT_MANAGER_FILE ) ); ?>/assets/images/sidebar_rate-plugin.gif" alt="<?php esc_html_e( 'Please Rate This Plugin At Wordpress.org!', 'multisite-robotstxt-manager' ); ?>" /></a></p>
 
 <div class="postbox">
 	<div class="h5 p-1 font-weight-bold"><?php esc_html_e( 'Robots.txt Help', 'multisite-robotstxt-manager' ); ?></div>
